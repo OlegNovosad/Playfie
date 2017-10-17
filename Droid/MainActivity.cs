00a6e14c632d2ad6df96aec3e3c2e7857bea2fcb
Okrekso <semikoseker@gmail.com>
2017-10-17 17:31:20 +0300

    clearing the files

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -17,19 +17,12 @@ namespace Playfie.Droid
             base.OnCreate(bundle);
 
             FacebookSdk.SdkInitialize(this.ApplicationContext);
-
-<<<<<<< HEAD
-<<<<<<< HEAD
             SetTheme(Android.Resource.Style.ThemeDeviceDefaultLight);
-=======
->>>>>>> parent of e97249a... theme fix
             SetContentView(Resource.Layout.login);
-=======
             LoginButton loginButton = (LoginButton)this.FindViewById(Resource.Id.login_button);
 
 
-            SetContentView(Resource.Layout.Main);
->>>>>>> parent of 5b58ad2... fix for facebook button
+            SetContentView(Resource.Layout.login);
             t = (TextView)FindViewById(Resource.Id.registrText);
             t.Click += Btn_Click;
         }
