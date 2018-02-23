edb576aa861ac369e0b97a843983fb24e63eaa7c
Oleg Novosad <onovosad@imprivata.com>
2018-02-23 16:52:52 +0200

    Test working build with profile fragment.

--- Droid/Views/Activities/MainActivity.cs
+++ Droid/Views/Activities/MainActivity.cs
@@ -5,15 +5,16 @@ using Android.Widget;
 using Android.OS;
 using Android.Content;
 using Android.App;
-using Android;
-using Android.Support.V4.Content;
+using static Android.Views.View;
+using Android.Views;
 
 namespace Playfie.Droid
 {
     [Activity(Label = "Playfie", Theme = "@style/splashscreen", ScreenOrientation = ScreenOrientation.Portrait)]
-    public class MainActivity : FragmentActivity
+    public class MainActivity : FragmentActivity, IOnClickListener
     {
         PlayfieMapFragment playfieMapFragment = new PlayfieMapFragment();
+        ImageButton btnUser;
         
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -23,16 +24,19 @@ namespace Playfie.Droid
             SetTheme(Android.Resource.Style.ThemeDeviceDefaultLightNoActionBar);
             SetContentView(Resource.Layout.Activity_Main);
 
-            if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.AccessFineLocation) == Permission.Denied)
-            {
-                RequestPermissions(new string[] { Manifest.Permission.AccessFineLocation, Manifest.Permission.WriteExternalStorage }, 11);
-            }
-            else
-            {
-                Android.Support.V4.App.FragmentTransaction transaction = SupportFragmentManager.BeginTransaction();
-                transaction.Add(Resource.Id.container, playfieMapFragment, playfieMapFragment.Class.SimpleName);
-                //transaction.Commit();
-            }
+            btnUser = (ImageButton)FindViewById(Resource.Id.userBtn);
+            btnUser.SetOnClickListener(this);
+
+            //if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.AccessFineLocation) == Permission.Denied)
+            //{
+            //    RequestPermissions(new string[] { Manifest.Permission.AccessFineLocation, Manifest.Permission.WriteExternalStorage }, 11);
+            //}
+            //else
+            //{
+            //    Android.Support.V4.App.FragmentTransaction transaction = SupportFragmentManager.BeginTransaction();
+            //    transaction.Add(Resource.Id.container, playfieMapFragment, playfieMapFragment.Class.SimpleName);
+            //    //transaction.Commit();
+            //}
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
@@ -47,6 +51,19 @@ namespace Playfie.Droid
             if (requestCode == 12 && resultCode == Result.Ok)
             {
 
+            }
+        }
+
+        public void OnClick(View v)
+        {
+            switch (v.Id)
+            {
+                case Resource.Id.userBtn:
+                    Android.Support.V4.App.FragmentTransaction transaction = SupportFragmentManager.BeginTransaction();
+                    transaction.Add(Resource.Id.container, new ProfileFragment());
+                    transaction.Commit();
+                    break;
+                default: break;
             }
         }
     }
